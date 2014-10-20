13b6886a5ff078e3e94a03bbb4d5a3dafd310646
寺内 信夫 <tenobu@gmail.com>
2014-10-20 12:21:02 +0900

    Initial Commit

---
+++ Map/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Map
+//
+//  Created by 寺内 信夫 on 2014/10/20.
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
